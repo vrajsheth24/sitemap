256a1a49956130286bcab7471bbcc0307193e23f
vrajsheth24 <vraj.empbweb@gmail.com>
2026-09-10 17:12:15 +0530

    chore: remove unused configuration file

--- scratch/test_queue.mjs
+++
@@ -1,7 +0,0 @@
-// Test queue logic for ClientCrawler
-const rootUrl = 'https://www.phrtax.cpa/';
-const visited = new Set();
-const enqueued = new Set([rootUrl]);
-const queue = [{ url: rootUrl, depth: 0 }];
-
-console.log('Queue initialized:', queue.length);
